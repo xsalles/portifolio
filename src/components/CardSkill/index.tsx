df705833917cad89e31c0175b1755db8e86feba2
xsalles <xs.salles@gmail.com>
2025-07-16 14:51:37 -0300

    refactored: now I use any lib of icons to put the image.

--- src/components/CardSkill/index.tsx
+++ src/components/CardSkill/index.tsx
@@ -1,14 +1,15 @@
 import Image from "next/image";
+import { ReactNode } from "react";
 
 interface CardSkillProps {
-  imageSrc: string;
+  imageSrc: ReactNode;
   nameSkil: string;
 }
 
 export default function CardSkill({imageSrc, nameSkil}: CardSkillProps) {
   return (
     <article className="flex w-46 h-46 flex-col items-center justify-center p-6 gap-8 bg-background hover:bg-foreground text-foreground hover:text-background transition-colors duration-300 ease-in-out border-2 border-foreground rounded-sm">
-      <Image src={imageSrc} alt={`Ícone da habilidade ${nameSkil}`} width={56} height={56}/>
+      {imageSrc}
       <h5 className="text-heading-h5 font-bold">{nameSkil}</h5>
     </article>
   );
--- src/template/LandingPage/index.tsx
+++ src/template/LandingPage/index.tsx
@@ -1,12 +1,13 @@
 import CardSkill from "@/components/CardSkill";
 import HeroSection from "./sections/HeroSection";
+import { IoLogoJavascript } from "react-icons/io5";
 
 
 export default function LandingPage() {
   return (
     <main className=" h-full">
         <HeroSection />
-        <CardSkill imageSrc="/logo-white.svg" nameSkil="Teste"/>
+        <CardSkill imageSrc={<IoLogoJavascript size={56}/>} nameSkil="Javascript"/>
     </main>
   )
 }
